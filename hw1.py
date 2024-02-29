ca0dd430a3e58af86b939d7a121d446419d885b8
LakshyaG42 <78310209+LakshyaG42@users.noreply.github.com>
2024-02-28 19:33:33 -0500

    done

--- hw1.py
+++ hw1.py
@@ -105,7 +105,7 @@ def filter_movies(d, thres_rating=3):
     # return: dictionary that maps movie to average rating
     # WRITE YOUR CODE BELOW
     thresholdMovies = {}
-    for key, value in d.values():
+    for key, value in d.items():
         if(value >= thres_rating):
             thresholdMovies[key] = value
     return thresholdMovies
@@ -137,6 +137,18 @@ def get_genre_rating(genre, genre_to_movies, movie_to_average_rating):
     # parameter movie_to_average_rating: dictionary  that maps movie to average rating
     # return: average rating of movies in genre
     # WRITE YOUR CODE BELOW
+    genreRating = 0.0
+    moviesInGenre = []
+
+    if genre in genre_to_movies.keys():
+        moviesInGenre.extend(genre_to_movies[genre])
+    else:
+        return -1
+    for movie in moviesInGenre:
+        if movie in movie_to_average_rating.keys():
+            genreRating += movie_to_average_rating[movie]
+    genreRating = genreRating / len(moviesInGenre)
+    return genreRating
     pass
     
 # 3.5
@@ -146,7 +158,11 @@ def genre_popularity(genre_to_movies, movie_to_average_rating, n=5):
     # parameter n: integer (for top n), default value 5
     # return: dictionary that maps genre to average rating
     # WRITE YOUR CODE BELOW
-    
+    genresToAvg = {}
+    for genre in genre_to_movies.keys():
+        genresToAvg[genre] = get_genre_rating(genre, genre_to_movies, movie_to_average_rating)
+
+    return get_popular_movies(genresToAvg, n)
     pass
 
 # ------ TASK 4: USER FOCUSED  --------
@@ -156,6 +172,17 @@ def read_user_ratings(f):
     # parameter f: movie ratings file name (e.g. "movieRatingSample.txt")
     # return: dictionary that maps user to list of (movie,rating)
     # WRITE YOUR CODE BELOW
+    UserRatings = {}
+    for line in open(f):
+        words = line.split("|")
+        title = words[0].strip()
+        rating = float(words[1].strip())
+        userid = int(words[2].strip())
+        if(userid in UserRatings.keys()):
+            UserRatings[userid].append((title, rating))
+        else:
+             UserRatings[userid] = [(title, rating)]
+    return UserRatings
     pass
     
 # 4.2
@@ -165,6 +192,21 @@ def get_user_genre(user_id, user_to_movies, movie_to_genre):
     # parameter movie_to_genre: dictionary that maps movie to genre
     # return: top genre that user likes
     # WRITE YOUR CODE BELOW
+    userMoviesToRatings = dict(user_to_movies[user_id]) 
+    #print(userMoviesToRatings)
+    userMovies_to_genre = {}
+    for key in userMoviesToRatings.keys():
+        userMovies_to_genre[key] = movie_to_genre[key]
+    #print(userMovies_to_genre)
+    userGenreToMovies = create_genre_dict(userMovies_to_genre)
+    #print(userGenreToMovies)
+    #allusersPopularGenres = genre_popularity(userGenreToMovies, userMoviesToRatings)
+    #print(allusersPopularGenres)
+    usersPopularGenre = genre_popularity(userGenreToMovies, userMoviesToRatings, 1)
+   #print(usersPopularGenre)
+    for key in usersPopularGenre.keys():
+        return key
+        
     pass
     
 # 4.3    
@@ -175,6 +217,17 @@ def recommend_movies(user_id, user_to_movies, movie_to_genre, movie_to_average_r
     # parameter movie_to_average_rating: dictionary that maps movie to average rating
     # return: dictionary that maps movie to average rating
     # WRITE YOUR CODE BELOW
+    userMoviesToRatings = dict(user_to_movies[user_id])
+    genreToMovies = create_genre_dict(movie_to_genre)
+    usersFavGenre = get_user_genre(user_id, user_to_movies, movie_to_genre)
+    movieRecs = {}
+    for movie in genreToMovies[usersFavGenre]:
+        if movie not in userMoviesToRatings.keys():
+            movieRecs[movie] = movie_to_average_rating[movie]
+    topMovieRecs = Counter(movieRecs)
+    top3movies = topMovieRecs.most_common(3)
+    return top3movies
+
     pass
 
 # -------- main function for your testing -----
@@ -185,8 +238,55 @@ def main():
     
     #create_genre_dict(read_movie_genre("genreMovieSample.txt"))
     #get_popular_movies(calculate_average_rating(read_ratings_data("movieRatingSample.txt")), 3)
-    print(get_popular_in_genre('Adventure', create_genre_dict(read_movie_genre('genreMovieSample.txt')),calculate_average_rating(read_ratings_data("movieRatingSample.txt"))))
+    
+    
+    #print(f"Movie Genres: {read_movie_genre('genreMovieSample.txt')}")
+    #print(f'Movie Average Ratings {calculate_average_rating(read_ratings_data("movieRatingSample.txt"))}')
+    #print(f"Adventure Avg Ratings: {get_genre_rating('Adventure', create_genre_dict(read_movie_genre('genreMovieSample.txt')),calculate_average_rating(read_ratings_data('movieRatingSample.txt')))}")
+    #print(f'All Genre Avg Ratings: {genre_popularity(create_genre_dict(read_movie_genre("genreMovieSample.txt")),calculate_average_rating(read_ratings_data("movieRatingSample.txt")))}')
+    #print(read_user_ratings("movieRatingSample.txt"))
+
+    #print(get_user_genre(6, read_user_ratings("movieRatingSample.txt"), read_movie_genre("genreMovieSample.txt")))
+
+
+    #Check with Friend:
+    movie_ratings_dict = read_ratings_data('movieRatingSample.txt')
+    #print(movie_ratings_dict)
+    
+    movie_genre_dict = read_movie_genre('genreMovieSample.txt')
+    #print(movie_genre_dict)
+    
+    cgd = create_genre_dict(movie_genre_dict)
+    #print(cgd)
+    
+    avgR = calculate_average_rating(movie_ratings_dict)
+    #print(avgR)
+    
+    popMov = get_popular_movies(avgR, 6)
+    #print(popMov)
+   
+    fm = filter_movies(avgR)
+    #print(fm)
+
+    gpg = get_popular_in_genre('Comedy', cgd, avgR)
+    #print(gpg)
+
+    ggr = get_genre_rating('Comedy', cgd, avgR)
+    #print(ggr)
+
+    gp = genre_popularity(cgd, avgR)
+    #print(gp)
+
+    rur = read_user_ratings('movieRatingSample.txt')
+    #print(rur)
+
+    gug = get_user_genre(15, rur, movie_genre_dict)
+    #print(gug)
+
+    rm = recommend_movies(15, rur, movie_genre_dict, avgR)
+    #print(rm)
     pass
+
     
 # DO NOT write ANY CODE (including variable names) outside of any of the above functions
 # In other words, ALL code your write (including variable names) MUST be inside one of
